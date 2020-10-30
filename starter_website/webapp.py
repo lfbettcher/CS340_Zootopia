3b1f05eb08debfda44c688a6534486373e04c54c
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2020-10-30 00:52:45 -0700

    add navbar

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -4,46 +4,14 @@ from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
 
-#provide a route where requests on the web application can be addressed
-@webapp.route('/hello')
-#provide a view (fancy name for a function) which responds to any requests on this route
-def hello():
-    return "Hello World!"
 
-@webapp.route('/browse_bsg_people')
-#the name of this function is just a cosmetic thing
-def browse_people():
-    print("Fetching and rendering people web page")
-    db_connection = connect_to_database()
-    query = "SELECT fname, lname, homeworld, age, id from bsg_people;"
-    result = execute_query(db_connection, query).fetchall()
-    print(result)
-    return render_template('people_browse.html', rows=result)
+@webapp.route('/')
+def index():
+    return render_template('index.html')
 
-@webapp.route('/add_new_people', methods=['POST','GET'])
-def add_new_people():
-    db_connection = connect_to_database()
-    if request.method == 'GET':
-        query = 'SELECT id, name from bsg_planets'
-        result = execute_query(db_connection, query).fetchall()
-        print(result)
 
-        return render_template('people_add_new.html', planets = result)
-    elif request.method == 'POST':
-        print("Add new people!")
-        fname = request.form['fname']
-        lname = request.form['lname']
-        age = request.form['age']
-        homeworld = request.form['homeworld']
-
-        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
-        data = (fname, lname, age, homeworld)
-        execute_query(db_connection, query, data)
-        return ('Person added!')
-
-@webapp.route('/browse_Animals')
-#the name of this function is just a cosmetic thing
-def browse_Animals():
+@webapp.route('/animals')
+def animals():
     print("Fetching and rendering Animals web page")
     db_connection = connect_to_database()
     query_animals = "SELECT animal_id, type, sex, name, age, weight, temperament, zookeeper_id FROM Animals;"
@@ -53,17 +21,18 @@ def browse_Animals():
     query_medications = "SELECT med_id, name FROM Medications;"
     result_medications = execute_query(db_connection, query_medications).fetchall()
     print(result_medications)
-    return render_template('Animals_browse.html', rows_animals=result_animals, rows_medications=result_medications)
+    return render_template('animals.html', rows_animals=result_animals, rows_medications=result_medications)
 
-@webapp.route('/add_new_animal', methods=['POST','GET'])
-def add_new_animal():
+
+@webapp.route('/add_new_animals', methods=['POST','GET'])
+def add_new_animals():
     db_connection = connect_to_database()
     if request.method == 'GET':
         query = 'SELECT animal_id, name from Animals'
         result = execute_query(db_connection, query).fetchall()
         print(result)
 
-        return render_template('Animals_add_new.html') # see people e.g.
+        return render_template('animals_add_new.html') # see people e.g.
     elif request.method == 'POST':
         print("Add new animal!")
         type = request.form['type']
@@ -89,9 +58,43 @@ def add_new_animal():
 #     print(result)
 #     return render_template('Medications_browse.html', rows=result)
 
-@webapp.route('/')
-def index():
-    return "<p>Are you looking for /db_test or /hello or <a href='/browse_bsg_people'>/browse_bsg_people</a> or /add_new_people or /update_people/id or /delete_people/id </p>"
+# @webapp.route('/')
+# def index():
+#     return "<p>Are you looking for /db_test or /hello or <a href='/browse_bsg_people'>/browse_bsg_people</a> or /add_new_people or /update_people/id or /delete_people/id </p>"
+
+
+@webapp.route('/browse_bsg_people')
+#the name of this function is just a cosmetic thing
+def browse_people():
+    print("Fetching and rendering people web page")
+    db_connection = connect_to_database()
+    query = "SELECT fname, lname, homeworld, age, id from bsg_people;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('people_browse.html', rows=result)
+
+
+@webapp.route('/add_new_people', methods=['POST','GET'])
+def add_new_people():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = 'SELECT id, name from bsg_planets'
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+
+        return render_template('people_add_new.html', planets = result)
+    elif request.method == 'POST':
+        print("Add new people!")
+        fname = request.form['fname']
+        lname = request.form['lname']
+        age = request.form['age']
+        homeworld = request.form['homeworld']
+
+        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
+        data = (fname, lname, age, homeworld)
+        execute_query(db_connection, query, data)
+        return ('Person added!')
+
 
 @webapp.route('/home')
 def home():
